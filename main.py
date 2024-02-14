461cdb2865c2d316b20c35f1a5203b908ceb2727
Jérôme Trips <jerome.trips@tutanota.com>
2024-02-14 17:46:37 +0100

    added function to get the actual image and save to a file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time # Keep track of time and sleep
-import requests # Make webrequests to apis
+import time  # Keep track of time and sleep
+import requests  # Make webrequests to apis
+
 
 def get_input():
     target_time: str = input("When should the timer go off? ")
@@ -14,7 +15,7 @@ def get_input():
         return hours, minutes
 
 
-#TODO: make this not freeze the whole thing while counting down
+# TODO: make this not freeze the whole thing while counting down
 def set_timer(hours, minutes) -> bool:
     current_time = time.strftime("%H:%M")
     target_time = f"{hours}:{minutes}"
@@ -24,21 +25,37 @@ def set_timer(hours, minutes) -> bool:
         print(current_time)
     return True
 
-def read_config(configfile)->dict:
-    '''reads necessary and optional configuration options from a file and returns them as a dict'''
-    
 
-def get_json_nasa(apikey :str="DEMO_KEY"):
-    """Grab image description from the nasa Astronomy image of the day, parse the returned json and return it. If Errors Occure, return the status code"""
-    response = requests.get("https://api.nasa.gov/planetary/apod",params={"api_key":apikey,"count":1})
+def read_config(configfile) -> dict:
+    """reads necessary and optional configuration options from a file and returns them as a dict"""
+
+
+def get_json_nasa(apikey: str = "DEMO_KEY"):
+    """Grab image description from the nasa Astronomy image of the day, parse the returned json and return it as a dict. If Errors Occure, return the status code"""
+    response = requests.get(
+        "https://api.nasa.gov/planetary/apod", params={"api_key": apikey, "count": 1}
+    )
     try:
-        response.raise_for_status()# raises HTTPError if requst was unsuccessfull
-        return response.json()
+        # raises HTTPError if requst was unsuccessfull
+        response.raise_for_status()
+        # the api sends a list of Json back as it could very well contain multiple images, However this request is structured to return
+        return response.json()[0]
     except requests.HTTPError:
         return response.status_code
+    
+def cache_image(imageinfo: dict):
+    if type(imageinfo) == dict:
+        imagedata = requests.get(imageinfo['url'])
+        filename = imageinfo['url'].spit("/")[-1]
+        print(filename)
+        with open(filename, 'wb') as f:
+            f.write(imagedata.content)
+    else:
+        print (imagedata.content)
         
-
-
+    
+    
+    
 def get_image_offline():
     """Get random image from a set of local folders
     return path to that image"""
@@ -55,15 +72,20 @@ def display_image(image):
     """get focus from Window Manager and display image Full Screen"""
     return None
 
+
 def error(exception: Exception):
-    '''handle Exceptions according to their nature'''
+    """handle Exceptions according to their nature"""
     print("Something went Wong")
 
+
 def main():
-    #hours, minutes = get_input()
-    #if set_timer(hours, minutes):
+    # hours, minutes = get_input()
+    # if set_timer(hours, minutes):
     #    print("Ready")
-    print(get_json_nasa())
+    info = get_json_nasa()
+    print(info)
+    #cache_image(info)
+
 
 if __name__ == "__main__":
     main()
