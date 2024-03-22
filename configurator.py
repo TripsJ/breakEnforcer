03b5c8b4520f122007ddf0b4be60b2e2c129cb8b
Jérôme Trips <jerome.trips@tutanota.com>
2024-03-22 15:53:49 +0100

    added pylintrc and added encoding to config filename

--- configurator.py
+++ configurator.py
@@ -1,16 +1,19 @@
-import toml
 import os.path
 import errno
+import toml
 
 
 class Configurator:
     def __init__(self, filepath="break.toml"):
         self.filepath = filepath  # path to configfile
+        self.config = {}
         # commandline arguments
 
     def read_configfile(self):
-        with open(self.filepath, "r") as f:
-            return toml.load(f)
+        with open(self.filepath, "r", encoding="utf-8") as file:
+            # open the config file using utf-8 as the encoding
+            # because it has the most characters and avoids relying on system defaults
+            return toml.load(file)
 
     @property
     def filepath(self):
@@ -20,6 +23,7 @@ class Configurator:
     def filepath(self, filepath):
         if os.path.isfile(os.path.abspath(filepath)):
             self._filepath = filepath
+            self.config = self.read_configfile()
         else:
             print("YAY I BROKE IT")
             raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), filepath)
