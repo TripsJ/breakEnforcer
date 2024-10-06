47b6fc4e63c73cb2c330273101a5226c8a94ae60
Jérôme Trips <jerome.trips@tutanota.com>
2024-10-06 03:53:12 +0200

    added test for invalid configfile handling in multiple cases. excluded testdatafrom checking by precommit

--- tests/test_configurator.py
+++ tests/test_configurator.py
@@ -4,6 +4,7 @@ import unittest.mock as mock
 import pytest
 
 import configurator
+from InvalidFileError import InvalidFileError
 
 
 @pytest.fixture
@@ -15,7 +16,7 @@ def test_read_valid_configfile(valid_configurator):
     assert valid_configurator.read_configfile() == {
         "storage": {"path": ".", "max": 1},
         "breaks": {"long": 30, "short": 5},
-        "work": {"interval": "40", "rounds": 3},
+        "work": {"interval": 40, "rounds": 3},
     }
 
 
@@ -23,8 +24,30 @@ def test_read_invalid_keys_configfile():
     pass
 
 
-def test_read_invalid_path_configfile():
-    pass
+@pytest.mark.parametrize(
+    "invalid_file, error",
+    [
+        ("./in/va/lid/nonexsistent.toml", FileNotFoundError),
+        ("testdata/invalid_extension.txt", InvalidFileError),
+        ("testdata/nonexsistent.toml", FileNotFoundError),
+        ("testdata/copyvalidasyaml.yaml", InvalidFileError),
+    ],
+)
+def test_read_invalid_configfile(invalid_file, error):
+
+    assert (
+        (not os.path.isfile(invalid_file))
+        or (
+            invalid_file == "testdata/invalid_extension.txt"
+            and os.path.isfile(invalid_file)
+        )
+        or (
+            invalid_file == "testdata/copyvalidasyaml.yaml"
+            and os.path.isfile(invalid_file)
+        )
+    )
+    with pytest.raises(error):
+        configurator.Configurator(invalid_file)
 
 
 def test_write_configfile(valid_configurator):
