a0101b37d4fc7ff17f21c7e46e4c83168f4f5810
Jérôme Trips <jerome.trips@tutanota.com>
2024-11-10 13:54:04 +0100

    -m

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,7 @@
 import unittest.mock as mock
 
 import pytest
+from PIL import Image as Im
 
 import main
 
@@ -37,15 +38,64 @@ nasa = "DEMO_KEY"
     )
 
 
-def test_get_conf() -> None:
-    pass
+@mock.patch("os.path.isfile")
+def test_get_conf(mock_os_is_file) -> None:
+    """Test fakes a valid config file .
+    Tests if the values and keys are read and returned correctly.
+    Returns:
+           None
+    Raises:
+           AssertionError"""
+
+    valid_data = """[storage]
+path = "."
+max = 2
+
+[breaks]
+long = 25
+short = 6
+
+[work]
+interval = 25
+rounds = 6
+
+[api.key]
+nasa = "DEMO_KEY"
+"""
+    mock_os_is_file.return_value = True
+    mock_open = mock.mock_open(read_data=valid_data)
+    with mock.patch("builtins.open", mock_open):
+        assert main.get_conf()["storage"]["path"] == "."
+        assert main.get_conf()["work"]["interval"] == 25
+        assert main.get_conf()["work"]["rounds"] == 6
+        assert main.get_conf()["api"]["key"]["nasa"] == "DEMO_KEY"
+        assert main.get_conf()["breaks"]["long"] == 25
+
+
+@mock.patch("os.path.isfile")
+def test_get_conf_not_found(mock_os_is_file) -> None:
+    """Test Forces a File not found Error on the config file, tests if default values are returned as expected
+    Returns:
+           None
+    Raises:
+           AssertionError"""
+    mock_os_is_file.return_value = False
+    assert main.get_conf()["storage"]["path"] == "."
+    assert main.get_conf()["work"]["interval"] == 40
+    assert main.get_conf()["work"]["rounds"] == 3
+    assert main.get_conf()["api"]["key"]["nasa"] == "DEMO_KEY"
+    assert main.get_conf()["breaks"]["long"] == 30
 
 
 ################ Image related tests ##########################################
 
 
-def test_get_monitor_size() -> None:
-    pass
+@mock.patch("PIL.ImageGrab.grab")
+def test_get_monitor_size(mock_monitor) -> None:
+
+    return_image = Im.new(mode="RGB", size=(1, 1))
+    mock_monitor.return_value = return_image
+    assert main.get_monitor_size() == (1, 1)
 
 
 def test_cache_image() -> None:
